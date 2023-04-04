421b359b100ec02c7d1354f68873f0324221a571
Aminul Islam <aminulds61@gmail.com>
2023-04-04 22:55:06 +0600

    Top channel data table continue...

--- public/js/index.js
+++ public/js/index.js
@@ -20,6 +20,7 @@ const salesChart = document.getElementById('salesChart');
 const purchaseChart = document.getElementById('purchaseChart');
 const pieChart = document.getElementById('pieChart');
 const polarArea = document.getElementById('polarArea');
+const topCountryChart = document.getElementById('topCountryChart');
 
 new Chart(salesChart, {
     type: 'bar',
@@ -104,6 +105,33 @@ new Chart(pieChart, {
 
 new Chart(polarArea, {
     type: 'polarArea',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June'],
+        datasets: [{
+            label: 'Number of Sales Return',
+            data: [10, 41, 19, 75, 18, 28],
+            backgroundColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54,162,235,1)',
+                'rgba(255,206,86,1)',
+                'rgba(75,192,192,1)',
+                'rgba(153,102,255,1)',
+                'rgba(255,159,64,1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+new Chart(topCountryChart, {
+    type: 'pie',
     data: {
         labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June'],
         datasets: [{
